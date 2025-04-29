4ad5a370c0eb98a484e94aca543b3a82f0a59e93
RBLG <37543249+RBLG@users.noreply.github.com>
2025-04-29 23:21:46 +0200

    init

---
+++ src/client/java/teluri/mods/jle/client/JustLikeErosionClient.java
@@ -0,0 +1,10 @@
+package teluri.mods.jle.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JustLikeErosionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
